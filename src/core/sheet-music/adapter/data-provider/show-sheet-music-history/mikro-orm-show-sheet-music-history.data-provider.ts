abaece24bae3b478c3a539cebcd04fc36deef290
Dominik <dominik.gasior@valueadd.pl>
2021-05-03 18:53:43 +0200

    Reduce returned properties

--- src/core/sheet-music/adapter/data-provider/show-sheet-music-history/mikro-orm-show-sheet-music-history.data-provider.ts
+++ src/core/sheet-music/adapter/data-provider/show-sheet-music-history/mikro-orm-show-sheet-music-history.data-provider.ts
@@ -3,7 +3,6 @@ import { SheetMusicHistoryResponseModel } from '../../../application/boundary/re
 import { EntityManager } from '@mikro-orm/postgresql';
 import { Injectable } from '@nestjs/common';
 import { SheetMusicInVersionEntity } from '../../repository/sheet-music-in-version/sheet-music-in-version.entity';
-import { MeasureResponseModel } from '../../../application/boundary/response-model/measure.response-model';
 import { MeasureResponseModelData } from '../measure-response-model-data';
 
 interface QueryResult {
@@ -20,32 +19,20 @@ export class MikroOrmShowSheetMusicHistoryDataProvider
   async provide(): Promise<SheetMusicHistoryResponseModel[]> {
     const results: QueryResult[] = await this.getQueryResults();
 
-    return results.map((result) => this.map(result));
+    return results.map((result) =>
+      MikroOrmShowSheetMusicHistoryDataProvider.map(result),
+    );
   }
 
   private getQueryResults(): Promise<QueryResult[]> {
     return this.entityManager
       .createQueryBuilder(SheetMusicInVersionEntity)
-      .select(['version', 'type', 'data'])
+      .select(['version', 'type'])
       .orderBy({ version: 'ASC' })
       .execute();
   }
 
-  private map(result: QueryResult): SheetMusicHistoryResponseModel {
-    const measures = Object.values(result.data.measures).map(
-      (data) =>
-        new MeasureResponseModel(
-          data.id,
-          data.clefType,
-          data.timeSignature,
-          Object.values(data.notes),
-        ),
-    );
-
-    return new SheetMusicHistoryResponseModel(
-      result.version,
-      result.type,
-      measures,
-    );
+  private static map(result: QueryResult): SheetMusicHistoryResponseModel {
+    return new SheetMusicHistoryResponseModel(result.version, result.type);
   }
 }
--- src/core/sheet-music/application/boundary/response-model/sheet-music-history.response-model.ts
+++ src/core/sheet-music/application/boundary/response-model/sheet-music-history.response-model.ts
@@ -1,9 +1,3 @@
-import { MeasureResponseModel } from './measure.response-model';
-
 export class SheetMusicHistoryResponseModel {
-  constructor(
-    public readonly version: number,
-    public readonly type: string,
-    public readonly measures: MeasureResponseModel[],
-  ) {}
+  constructor(public readonly version: number, public readonly type: string) {}
 }
